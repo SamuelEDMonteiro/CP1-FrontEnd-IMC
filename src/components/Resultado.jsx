0d7eb07f65493b271f783e2a66fca5c1db70f012
samuel <labsfiap@fiap.com.br>
2025-09-04 10:21:10 -0300

    arrumando a Tabela

--- src/components/Resultado.jsx
+++ src/components/Resultado.jsx
@@ -23,5 +23,5 @@ function Resultado({resultado}){
 
         </div>
     )
-}
+};
 export default Resultado
--- src/components/TabelaImc.jsx
+++ src/components/TabelaImc.jsx
@@ -1,27 +1,42 @@
-import '../css/resultado.css'
-import TabelaImc from './TabelaImc'
+import "../css/resultado.css"
 
-//destruct - desestruturação- acessa os dados de outro componente
-function Resultado({resultado}){
-    return(
-        <div className='container'>
-            <div className='result'>
-                <h2>Seu IMC é de: <span className='imc-span'>{resultado}</span></h2>
-            </div>
-
-            <table className='classification'>
-                <thead className='tabelaHeader'>
-                    <tr>
-                        <th>Classificação</th>
-                        <th>IMC</th>
-                    </tr>
-                </thead>
-                <tbody className='tabelaBody'>
-                    {TabelaImc(resultado)}
-                </tbody>
-            </table>
-
-        </div>
-    )
+const TabelaImc=(resultado)=>{
+    if(resultado<18){
+        return (
+            <>
+            <td>Abaixo do peso</td>
+            <td>Abaixo do 18</td>
+            </>
+        );
+    }else if (resultado >18 && resultado <25){
+        return(
+            <>
+            <td>Peso Normal</td>
+            <td>18 - 24.9</td>
+            </>
+        );
+    }else if(resultado>24 && resultado<30){
+        return(
+            <>
+            <td>Sobre Peso</td>
+            <td>25 - 29.9</td>
+            </>
+        );
+    }else if (resultado > 29 && resultado <35){
+        return(
+            <>
+            <td>Obesidade Grau-1</td>
+            <td>30 - 34.9</td>
+            </>
+        );
+    }else{
+        return(
+            <>
+            <td>Obesidade Mórbida</td>
+            <td>Maior ou a 40</td>
+            </>
+        );
+    }
 }
-export default Resultado
+
+export default TabelaImc     
