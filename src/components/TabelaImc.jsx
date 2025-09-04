4d74318f46e99eb08b619a72eea115113c7f34c0
samuel <labsfiap@fiap.com.br>
2025-09-04 08:06:00 -0300

    criando a estrtutura do Resultado e da Tabela

---
+++ src/components/TabelaImc.jsx
@@ -0,0 +1,27 @@
+import '../css/resultado.css'
+import TabelaImc from './TabelaImc'
+
+//destruct - desestruturação- acessa os dados de outro componente
+function Resultado({resultado}){
+    return(
+        <div className='container'>
+            <div className='result'>
+                <h2>Seu IMC é de: <span className='imc-span'>{resultado}</span></h2>
+            </div>
+
+            <table className='classification'>
+                <thead className='tabelaHeader'>
+                    <tr>
+                        <th>Classificação</th>
+                        <th>IMC</th>
+                    </tr>
+                </thead>
+                <tbody className='tabelaBody'>
+                    {TabelaImc(resultado)}
+                </tbody>
+            </table>
+
+        </div>
+    )
+}
+export default Resultado
