c2902511e4c020d34c85bc1838ab217c42392f88
samuel <labsfiap@fiap.com.br>
2025-09-04 08:15:32 -0300

    adicionando links no Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,11 +8,11 @@ function Footer() {
                 <a href="#"><img src={logo} alt="logo IMC" /></a>
             </div>
             <div className="social-links">
-                <a href="" target='_blank'><i className='fa-brands fa-instagram'></i></a>
-                <a href="" target='_blank'><i className='fa-brands fa-linkedin-in'></i></a>
-                <a href="" target='_blank'><i className='fa-brands fa-github'></i></a>
+                <a href="https://www.instagram.com/smk_dmz" target='_blank'><i className='fa-brands fa-instagram'></i></a>
+                <a href="https://www.linkedin.com/in/samuel-monteiro-5b0184325" target='_blank'><i className='fa-brands fa-linkedin-in'></i></a>
+                <a href="https://github.com/SamuelEDMonteiro" target='_blank'><i className='fa-brands fa-github'></i></a>
             </div>
-            <p>IMC &copy;2025 - Todos os direitos reservados. <a href="" target='_blank'>Samuel Monteiro</a></p>
+            <p>IMC &copy;2025 - Todos os direitos reservados. <a href="https://www.linkedin.com/in/samuel-monteiro-5b0184325" target='_blank'>Samuel Monteiro</a></p>
         </footer>
     )
 }
